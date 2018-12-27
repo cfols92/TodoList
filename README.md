5ac1f69c027d55766ad818e84555e955e1235cdb
Chris Folsom <cfols92@gmail.com>
2018-12-27 18:44:17 -0500

    added all files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TodoList
 
-Use HTML, CSS, and JavaScript (with jQuery) to create a to-do list application.
+Used HTML, CSS, and JavaScript (with jQuery) to create a to-do list application.
 
 Add tasks, cross them off when completed, and delete tasks from the list when you no longer wish to see them.
---
+++ assets/js/todos.js
@@ -0,0 +1,38 @@
+//check off specific to-dos by clicking
+
+$("ul").on("click", "li", function()
+{
+    $(this).toggleClass("completed")
+});
+
+//delete to-dos by clicking on X's
+
+$("ul").on("click", "span", function(event)
+{
+    event.stopPropagation();
+    $(this).parent().fadeOut(500,function()
+    {
+        $(this).remove();
+    })
+});
+
+//add new to-dos through the text input
+
+$("input[type='text']").keypress(function(event)
+{
+    if(event.which === 13)
+    {
+        //get text from input
+        todoText = $(this).val();
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class=\"fas fa-trash-alt\"></i></span> " + todoText + "</li>");
+        //clear text input
+        $(this).val("");
+    }
+});
+
+//toggle the input using the plus icon
+$(".fa-plus").click(function()
+{
+    $("input[type='text']").fadeToggle();
+});
